fc44c7ec5610f937241dbc9bca842c54730183e3
Kamil Szmagier (CODECOOL POLAND SP. Z O.O.) <PXW473@motorolasolutions.com>
2022-06-02 10:12:04 +0200

    Add method to check if all cards values are in bounds

--- tddDojo/Poker.cs
+++ tddDojo/Poker.cs
@@ -1,14 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 namespace tddDojo
 {
     public class Poker
     {
         private const int CorrectNumberOfCards = 5;
+        private const int MinCardValue = 1;
+        private const int MaxCardValue = 6;
 
         public string PokerHand(int[] cardsInHand)
         {
-            if (!IsCardsInHandCountIsCorrect(cardsInHand))
+            if (!IsCardsInHandCountIsCorrect(cardsInHand) || !IsAnyCardInHandIsOutOfBounds(cardsInHand))
                 throw new ArgumentException();
 
             return string.Empty;
@@ -17,6 +20,13 @@ namespace tddDojo
         private bool IsCardsInHandCountIsCorrect(int[] cardsInHand)
         {
             if (cardsInHand.Length != CorrectNumberOfCards)
+                return false;
+            else return true;
+        }
+
+        private bool IsAnyCardInHandIsOutOfBounds(int[] cardsInHand)
+        {
+            if (cardsInHand.Any(cardValue => cardValue < MinCardValue || cardValue > MaxCardValue))
                 return false;
             else return true;
         }
