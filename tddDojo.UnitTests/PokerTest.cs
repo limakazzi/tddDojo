ce8c8d0640e5e7c0010024953baa584e6065573c
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-06-02 10:05:14 +0200

    var3

--- tddDojo.UnitTests/PokerTest.cs
+++ tddDojo.UnitTests/PokerTest.cs
@@ -14,5 +14,21 @@ namespace tddDojo.UnitTests
 
             Assert.Throws<ArgumentException>(() => poker.PokerHand(cardsInHand));
         }
+
+        [Test]
+        public void PokerHand_WhenCardsInHandHaveTooBigValue_ShouldThrowArgumentException()
+        {
+            var poker = new Poker();
+            var cardsInHand = new[] { 2, 4, 5, 9, 1 };
+            Assert.Throws<ArgumentException>(() => poker.PokerHand(cardsInHand));
+        }
+        [Test]
+        public void PokerHand_WhenCardsInHandHaveTooLowValue_ShouldThrowArgumentException()
+        {
+            var poker = new Poker();
+            var cardsInHand = new[] { 2, 0, 5, 3, 1 };
+            Assert.Throws<ArgumentException>(() => poker.PokerHand(cardsInHand));
+        }
     }
 }
+
