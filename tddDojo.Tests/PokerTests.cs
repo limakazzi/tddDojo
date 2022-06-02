9df0d3060b53349d97df6348d3c545d2183c7f8c
Kamil Szmagier (CODECOOL POLAND SP. Z O.O.) <PXW473@motorolasolutions.com>
2022-06-02 09:42:40 +0200

    Add first unit test

---
+++ tddDojo.Tests/PokerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tddDojo.Tests
+{
+    [TestFixture]
+    public class PokerTests
+    {
+        [Test]
+        public void PokerHand_WhenCardsInHandCountIsNotFive_ShouldThrowArgumentException()
+        {
+            var poker = new Poker();
+            var cardsInHand = new[] { 1, 2, 5 };
+
+            Assert.Throws<ArgumentException>(() => poker.PokerHand(cardsInHand));
+        }
+    }
+}
---
+++ tddDojo/Poker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tddDojo
+{
+    public class Poker
+    {
+        public string PokerHand(int[] cardsInHand)
+        {
+            return string.Empty;
+        }
+    }
+}
